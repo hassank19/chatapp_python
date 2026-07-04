64483b1d960c7b9a9f2a14ebf1459c9c4a26872a
Hassaan <hassankhalids2007@gmail.com>
2026-07-04 21:30:01 +0530

    Added input validation, function to get rid of repeated code

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ from classes import User
 from classes import Message
 from datetime import datetime
 
+
+
 def search_msg(sender, reciever):
     current_user = sender.username
     current_reciever = reciever.username
@@ -11,11 +13,11 @@ def search_msg(sender, reciever):
     try:
         with open("messageinfo.txt", "r") as chatfile:
             for line in chatfile:
-                parts = line.strip().split(", ")
-                msg_sender = parts[1].replace("From: ", "")
-                msg_reciever = parts[2].replace("To: ", "")
-                my_text = parts[3].replace("Text: ", "")
-                if ((current_user == msg_sender and current_reciever == msg_reciever) or (current_user == msg_reciever and current_reciever == msg_sender)) and string in my_text:
+                parts = line.strip().split("|")
+                msg_sender = parts[1]
+                msg_reciever = parts[2]
+                msg_text = parts[3]
+                if ((current_user == msg_sender and current_reciever == msg_reciever) or (current_user == msg_reciever and current_reciever == msg_sender)) and string in msg_text:
                     found = True
                     search_result.append(f"{line}\n")    
             if found:
@@ -33,7 +35,7 @@ def send_msg(sender, receiver):
     message = Message(timestamp, sender, receiver, my_text)
     #message1.get_info()
     with open("messageinfo.txt", "a") as msginfo:
-        msginfo.write(f"Time: {message.time}, From: {message.isfrom.username}, To: {message.isto.username}, Text: {message.text}\n")
+        msginfo.write(f"{timestamp}|{sender.username}|{receiver.username}|{message.text}\n")
     print("---------------")
 
 
@@ -46,10 +48,10 @@ def view_history(sender, receiver):
     try:
         with open("messageinfo.txt", "r") as msginfo:
             for line in msginfo:
-                parts = line.strip().split(", ")
+                parts = line.strip().split("|")
                 if len(parts) >= 4: #if there are at least 4 parts in list or more like time, sender, receiver, text, only then continue, if less then something is wrong
-                    msg_sender = parts[1].replace("From: ", "") #remove the from from the 2nd part of list, and only keep the username string
-                    msg_reciever = parts[2].replace("To: ", "") #same here
+                    msg_sender = parts[1] #remove the from from the 2nd part of list, and only keep the username string
+                    msg_reciever = parts[2] #same here
                     if (current_user == msg_sender and current_reciever == msg_reciever) or (current_user == msg_reciever and current_reciever == msg_sender): #if the username of the current sender and receiver object matches the sent from and is to names in the file then only it will print
                         print(f"{line}\n")
                         print("---------------")
@@ -68,10 +70,10 @@ def delete_messages(sender, reciever):
     try:
         with open("messageinfo.txt", "r") as chatfile:
             for line in chatfile:
-                parts = line.strip().split(", ")
+                parts = line.strip().split("|")
                 if len(parts) >= 4:
-                    msg_sender = parts[1].replace("From: ", "")
-                    msg_reciever = parts[2].replace("To: ", "")
+                    msg_sender = parts[1]
+                    msg_reciever = parts[2]
                     if (current_sender == msg_sender and current_reciever == msg_reciever) or (current_sender == msg_reciever and current_reciever == msg_sender):
                         msg_found = True
                         continue
@@ -88,78 +90,73 @@ def delete_messages(sender, reciever):
     except FileNotFoundError:
         print("Something went wrong")
 
+def find_reciever(sender):
+    #for loop to make the user choose the person they want to send the message
+    for x in range(len(User.users)): #just take the length of the list so that we can enumerate it (it will make it easy to find sender and receiver based on index)
+        if (sender.username == User.users[x].username):
+            print(f"{x+1}: {User.users[x].username} (You)") #line x: if the current user is found in the list users, it will just print the option to message yourself, and then conitnue after incrementing
+            continue
+        print(f"{x+1}: {User.users[x].username}")
+    choice = int(input("Enter the number: "))
+    reciever_ = User.users[choice-1] #reciever here is storing an object, -1 because indexing starts from 0 
+    return reciever_
+
+
 def after_login(sender):
     #for user in User.users:
         #print(f"{user.username}") #couldve done this aswell but itd be very limited
     print("Choose an operation: ")
     print("---------------")
     while True:
-        operation = int(input("1: Send a message \n2: View chat history \n3: Delete messages \n4: Search messages \n5: Exit \n---------------\n")) #menu based operations
-        if (operation == 1):
-            print("Send a message to:\n")
-            #for loop to make the user choose the person they want to send the message
-            for x in range(len(User.users)): #just take the length of the list so that we can enumerate it (it will make it easy to find sender and receiver based on index)
-                if (sender.username == User.users[x].username):
-                    print(f"{x+1}: {User.users[x].username} (You)") #if the current user is found in the list users, it will just print the option to message yourself, and then conitnue after incrementing
-                    continue
-                print(f"{x+1}: {User.users[x].username}")
-            choice = int(input("Enter the number: "))
-            reciever = User.users[choice-1] #reciever here is storing an object 
-            send_msg(sender, reciever)
+        try: 
+            operation = int(input("1: Send a message \n2: View chat history \n3: Delete messages \n4: Search messages \n5: Exit \n---------------\n")) #menu based operations
+            if (operation == 1):
+                print("Send a message to:\n")
+                reciever = find_reciever(sender) #theres only one point of passing sender as a value, that is line x in find reciever funtion
+                send_msg(sender, reciever)
 
-        elif (operation == 2):
-            print("View your chat history with: ")
-            for x in range(len(User.users)): #just take the length of the list so that we can enumerate it (it will make it easy to find sender and receiver based on index)
-                if (sender.username == User.users[x].username):
-                    print(f"{x+1}: {User.users[x].username} (You)") #if the current user is found in the list users, it will just print the option to message yourself, and then conitnue after incrementing
-                    continue
-                print(f"{x+1}: {User.users[x].username}")
-            choice = int(input("Enter the number: "))
-            reciever = User.users[choice-1] #reciever here is storing an object 
-            view_history(sender, reciever)
+            elif (operation == 2):
+                print("View your chat history with: ")
+                reciever = find_reciever(sender)
+                view_history(sender, reciever)
 
-        elif (operation == 3):
-            print("Delete a message in your chat with: ")
-            for x in range(len(User.users)):
-                if (sender.username == User.users[x].username):
-                    print(f"{x+1}: {User.users[x].username} (You)") #if the current user is found in the list users, it will just print the option to message yourself, and then conitnue after incrementing
-                    continue
-                print(f"{x+1}: {User.users[x].username}")
-            choice = int(input("Enter the number: "))
-            reciever = User.users[choice-1]
-            delete_messages(sender, reciever)
+            elif (operation == 3):
+                print("Delete a message in your chat with: ")
+                reciever = find_reciever(sender)
+                delete_messages(sender, reciever)
 
-        elif (operation == 4):
-            print("Search messages with: ")
-            for x in range(len(User.users)):
-                if (sender.username == User.users[x].username):
-                    continue
-                print(f"{x+1}: {User.users[x].username}")
-            choice = int(input("Enter the number: "))
-            reciever = User.users[choice-1]
-            search_msg(sender, reciever)
+            elif (operation == 4):
+                print("Search messages with: ")
+                reciever = find_reciever(sender)
+                search_msg(sender, reciever)
 
 
-        elif (operation == 5):
-            print("---------------")
-            print("Exited")
-            print("---------------")
-            break
+            elif (operation == 5):
+                print("---------------")
+                print("Exited")
+                print("---------------")
+                break
+        except ValueError:
+            print("Invalid Input")
 
 #-----------------------------------------------------
 def find_user_index_inlist(uid):
     index = 0
+    found = False
     for x in range(len(User.users)):
         if uid == User.users[x].username:
             index = x
+            found = True
             break
-    return index
-
+    if found:
+        return index
+    else: #index value is set to 0, its not good to return 0 thus we can return none
+        return None
 
 
 def login():
-    user_id = input("Enter your username: ")
-    password = input("Enter a password: ")
+    user_id = input("Enter your username: ").strip()
+    password = input("Enter a password: ").strip()
     found = False
     with open("udatabase.txt", "r") as database:
         for line in database:
@@ -180,8 +177,8 @@ def login():
 
 def sign_up():
 
-    user_id = input("Enter a username: ")
-    password = input("Make a password: ")
+    user_id = input("Enter a username: ").strip()
+    password = input("Make a password: ").strip()
     found = False
     with open("udatabase.txt", "r") as database:
         for line in database:
@@ -215,14 +212,15 @@ def load_users(): #whenever u run the code, the new users created after signing 
 
 
 load_users()
-ui = int(input("1: Sign Up\n2: Login \nChoose an operation: "))
-if ui == 1:
-    sign_up()
-if ui == 2:
-    login()
 
-#login_ui = int(input("Enter the number to login: "))
-#print("---------------")
-#sender_temp = User.users[login_ui - 1] #this is storing the object at that index
-
-#after_login(sender_temp)
+while True:
+    try:
+        ui = int(input("1: Sign Up\n2: Login\n3: Exit\nChoose an operation: "))
+        if ui == 1:
+            sign_up()
+        elif ui == 2:
+            login()
+        elif ui == 3:
+            break
+    except ValueError:
+        print("Invalid Input")
